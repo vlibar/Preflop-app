5e5aecf2e61110a10945d6be86c2dc79b144e17f
Vladislav Baranov <sjp5896@gmail.com>
2025-04-26 19:30:41 +0300

    lasd3

--- app.py
+++ app.py
@@ -9,7 +9,34 @@ CORS(app, origins=["https://sosik.pythonanywhere.com"])
 @app.route('/invoke', methods=['POST'])
 def invoke():
     data = request.get_json(force=True)
-    result = get_rfi_action(**data)
+    action = data.get('action')
+
+    if action == 'rfi':
+        position = data.get('position')
+        hole_cards = data.get('hole_cards')
+
+        result = get_rfi_action(position=position,
+                                hole_cards=hole_cards)
+
+    elif action == 'facing_rfi':
+        your_position = data.get('your_position')
+        rfi_position = data.get('rfi_position')
+        hole_cards = data.get('hole_cards')
+        result = get_facing_rfi_action(your_position=your_position,
+                                       rfi_position=rfi_position,
+                                       hole_cards=hole_cards)
+
+    elif action == 'rfi_vs_3bet':
+        your_position = data.get('your_position')
+        three_bet_position = data.get('three_bet_position')
+        hole_cards = data.get('hole_cards')
+        result = get_rfi_vs_3bet_action(your_position=your_position,
+                                       three_bet_position=three_bet_position,
+                                       hole_cards=hole_cards)
+
+    else:
+        return jsonify({'error': f'Unknown action: {action}'}), 400
+
     return jsonify({'result': result})
 
 if __name__ == '__main__':
